78c94328aaea05d02f27249fbb939fc68acdf594
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2025-11-18 00:46:51 +0700

    2

--- sushi-concept-website-template/src/order.jsx
+++ sushi-concept-website-template/src/order.jsx
@@ -40,85 +40,105 @@ export default function Order() {
   
                     {/* 1 */}
                     <div>
-                        <div className="order1">
-                            <Link to="/item1"><img src="https://www.educatepark.com/wp-content/uploads/2015/01/Image.jpg" alt="" /></Link>
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item1")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order1">
+                                <Link to="/item1"><img src="https://www.educatepark.com/wp-content/uploads/2015/01/Image.jpg" alt="" /></Link>
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.   <br />  <br />
+                            <button className="btnorder" onClick={() => (window.location.href = "/item1")}><p>ORDERNOW</p></button></div> 
+                        </center>
               
                       
                     </div>
 
                     {/* 2 */}
                     <div>
-                        <div className="order2">
-                            <Link to="/item2"><img src="/sushirow.png" alt="" /></Link>
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item2")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order2">
+                                <Link to="/item2"><img src="/sushirow.png" alt="" /></Link>
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.  <br /><br />   <button className="btnorder" onClick={() => (window.location.href = "/item2")}><p>ORDERNOW</p></button></div> 
+                        </center>
                     </div>
 
                     {/* 3 */}
                     <div>
-                        <div className="order3">
-                           <Link to="/item3"><img src="/sushisalmon.png" alt="" /></Link> 
+                        <center>
+                            <div className="order3">
+                            <Link to="/item3"><img src="/sushisalmon.png" alt="" /></Link> 
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur. <br /><br />    <button className="btnorder" onClick={() => (window.location.href = "/item3")}><p>ORDERNOW</p></button></div> 
+                        </center>
                         </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item3")}><p>สั่งสินค้า</p></button></div> 
-                    </div>
 
                     {/* 4 */}
                     <div>
-                        <div className="order4">
-                            <Link to="/item4"><img src="/image.png" alt="" /></Link>
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item4")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order4">
+                                <Link to="/item4"><img src="/image.png" alt="" /></Link>
+                            </div>
+                            <br /><br />
+                         
+                               <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.  <br /><br />   <button className="btnorder" onClick={() => (window.location.href = "/item4")}><p>ORDERNOW</p></button></div> 
+                        </center>
                     </div>
 
                     {/* 5 */}
                     <div>
-                        <div className="order5">
-                            <Link to="/item5"><img src="/ssushi.jpg" alt="" /></Link>
+                        <center>
+                            <div className="order5">
+                                <Link to="/item5"><img src="/ssushi.jpg" alt="" /></Link>
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur. <br /><br />    <button className="btnorder" onClick={() => (window.location.href = "/item5")}><p>ORDERNOW</p></button></div> 
+                        </center>
                         </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item5")}><p>สั่งสินค้า</p></button></div> 
-                    </div>
 
                     {/* 6 */}
                     <div>
-                        <div className="order6">
-                           <Link to="/item6"><img src="/rice.jpg" alt="" /></Link> 
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item6")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order6">
+                            <Link to="/item6"><img src="/rice.jpg" alt="" /></Link> 
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.  <br /><br />   <button className="btnorder" onClick={() => (window.location.href = "/item6")}><p>ORDERNOW</p></button></div> 
+                        </center>
                     </div>
 
                     {/* 7 */}
                     <div>
-                        <div className="order7">
-                           <Link to="/item7"><img src="https://i.ytimg.com/vi/_SJfXONxEz8/hq720.jpg" alt="" /></Link> 
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item7")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order7">
+                            <Link to="/item7"><img src="https://i.ytimg.com/vi/_SJfXONxEz8/hq720.jpg" alt="" /></Link> 
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.  <br /><br />   <button className="btnorder" onClick={() => (window.location.href = "/item7")}><p>ORDERNOW</p></button></div> 
+                        </center>
                     </div>
 
                     {/* 8 */}
                     <div>
-                        <div className="order8">
-                            <Link to="/item8"><img src="/reme.jpg" alt="" /></Link>
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item8")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order8">
+                                <Link to="/item8"><img src="/reme.jpg" alt="" /></Link>
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.   <br /><br />  <button className="btnorder" onClick={() => (window.location.href = "/item8")}><p>ORDERNOW</p></button></div> 
+                        </center>
                     </div>
 
                     {/* 9 */}
                     <div>
-                        <div className="order9">
-                            <Link to="/item9"><img src="/jp-food-1.jpg" alt="" /></Link>
-                        </div>
-                        <br /><br />
-                        <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.     <button className="btnorder" onClick={() => (window.location.href = "/item")}><p>สั่งสินค้า</p></button></div> 
+                        <center>
+                            <div className="order9">
+                                <Link to="/item9"><img src="/jp-food-1.jpg" alt="" /></Link>
+                            </div>
+                            <br /><br />
+                            <div style={{ color: "aliceblue" }}>Lorem ipsum dolor sit amet consectetur.  <br /><br />   <button className="btnorder" onClick={() => (window.location.href = "/item")}><p>ORDERNOW</p></button></div> 
+                        </center>
                     </div>
 
                 </div>
