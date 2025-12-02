b683af61eaf4d3c15fb64db7f39d5114b1b41a7c
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2025-12-02 23:53:22 +0700

    2

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -5,11 +5,26 @@ const Post = require('../model/User')
 // ! โชว์ข้อมูลทั้งหมดไปให้  Font-end
 const getPosts = async (req , res) =>{
     try{
-        const post =  await Post.find()
+        const posts =  await Post.find()
 
-        res.status(200).json({post})
+        res.status(200).json({posts})
 
     }catch(error){
        res.status(404).json({message: error.message})
+    }
+}
+
+
+
+
+const  createPost = async (req , res) =>{
+    const post = req.body
+    const newPost = new Post(post)
+    try{
+        await newPost.save()
+        res.status(201).json(newPost)
+
+    }catch(error){
+        res.status(409).json({message: error.message})
     }
 }
